b2f00b9a6ccadf4695beee932aa6af5881848d87
sendo-kakeru <sendokakeru.js@gmail.com>
2024-09-15 06:34:27 +0900

    add build

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,6 +1,118 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
+const my_connection = new gcp.cloudbuildv2.Connection("my-connection", {
+    location: "us-central1",
+    name: "my-connection",
+    githubConfig: {
+        appInstallationId: 123123,
+        authorizerCredential: {
+            oauthTokenSecretVersion: "projects/my-project/secrets/github-pat-secret/versions/latest",
+        },
+    },
+});
+const my_repository = new gcp.cloudbuildv2.Repository("my-repository", {
+    name: "my-repo",
+    parentConnection: my_connection.id,
+    remoteUri: "https://github.com/sendo-kakeru/pulumi-cloud-run",
+});
+
+const build_trigger = new gcp.cloudbuild.Trigger("build-trigger", {
+    name: "my-trigger",
+    location: "global",
+    repositoryEventConfig: {
+        repository: my_repository.id,
+        push: {
+            branch: "main",
+        },
+    },
+    build: {
+        steps: [
+            {
+                name: "gcr.io/cloud-builders/gsutil",
+                args: [
+                    "cp",
+                    "gs://mybucket/remotefile.zip",
+                    "localfile.zip",
+                ],
+                timeout: "120s",
+                secretEnvs: ["MY_SECRET"],
+            },
+            {
+                name: "ubuntu",
+                script: "echo hello",
+            },
+        ],
+        source: {
+            storageSource: {
+                bucket: "mybucket",
+                object: "source_code.tar.gz",
+            },
+        },
+        tags: [
+            "build",
+            "newFeature",
+        ],
+        substitutions: {
+            _FOO: "bar",
+            _BAZ: "qux",
+        },
+        queueTtl: "20s",
+        logsBucket: "gs://mybucket/logs",
+        secrets: [{
+            kmsKeyName: "projects/myProject/locations/global/keyRings/keyring-name/cryptoKeys/key-name",
+            secretEnv: {
+                PASSWORD: "ZW5jcnlwdGVkLXBhc3N3b3JkCg==",
+            },
+        }],
+        availableSecrets: {
+            secretManagers: [{
+                env: "MY_SECRET",
+                versionName: "projects/myProject/secrets/mySecret/versions/latest",
+            }],
+        },
+        artifacts: {
+            images: ["gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA"],
+            objects: {
+                location: "gs://bucket/path/to/somewhere/",
+                paths: ["path"],
+            },
+            npmPackages: [{
+                packagePath: "package.json",
+                repository: "https://us-west1-npm.pkg.dev/myProject/quickstart-nodejs-repo",
+            }],
+            pythonPackages: [{
+                paths: ["dist/*"],
+                repository: "https://us-west1-python.pkg.dev/myProject/quickstart-python-repo",
+            }],
+            mavenArtifacts: [{
+                repository: "https://us-west1-maven.pkg.dev/myProject/quickstart-java-repo",
+                path: "/workspace/my-app/target/my-app-1.0.SNAPSHOT.jar",
+                artifactId: "my-app",
+                groupId: "com.mycompany.app",
+                version: "1.0",
+            }],
+        },
+        options: {
+            sourceProvenanceHashes: ["MD5"],
+            requestedVerifyOption: "VERIFIED",
+            machineType: "N1_HIGHCPU_8",
+            diskSizeGb: 100,
+            substitutionOption: "ALLOW_LOOSE",
+            dynamicSubstitutions: true,
+            logStreamingOption: "STREAM_OFF",
+            workerPool: "pool",
+            logging: "LEGACY",
+            envs: ["ekey = evalue"],
+            secretEnvs: ["secretenv = svalue"],
+            volumes: [{
+                name: "v1",
+                path: "v1",
+            }],
+        },
+    },
+});
+
 const _default = new gcp.cloudrun.Service("default", {
     name: "cloud-run-service-name",
     location: "us-central1",
@@ -46,52 +158,3 @@ const subscription = new gcp.pubsub.Subscription("subscription", {
         },
     },
 });
-
-// (async () => {
-//   const github_token_secret = await gcp.secretmanager.getSecretVersion({
-//     secret: "github-token-secret",
-//   });
-//   console.log(github_token_secret);
-//   const my_connection = new gcp.cloudbuildv2.Connection("my-connection", {
-//     location: "asia-northeast1",
-//     name: "my-connection",
-//     githubConfig: {
-//       // appInstallationId: 123123,
-//       authorizerCredential: {
-//         oauthTokenSecretVersion: github_token_secret.name,
-//         //   oauthTokenSecretVersion: github_token_secret.id,
-//       },
-//     },
-//   });
-//   const my_repository = new gcp.cloudbuildv2.Repository("my-repository", {
-//     location: "asia-northeast1",
-//     name: "my-repo",
-//     parentConnection: my_connection.name,
-//     remoteUri: "https://github.com/sendo-kakeru/pulumi-gcp-remix",
-//   });
-//   const repo_trigger = new gcp.cloudbuild.Trigger("repo-trigger", {
-//     location: "asia-northeast1",
-//     repositoryEventConfig: {
-//       repository: my_repository.id,
-//       push: {
-//         branch: "main",
-//       },
-//     },
-
-//     filename: "cloudbuild.yaml",
-//   });
-
-//   const cloudRunService = new gcp.cloudrun.Service("cloudRunService", {
-//     location: "asia-northeast1",
-//     template: {
-//       spec: {
-//         containers: [
-//           {
-//             //   image: repo_trigger.,
-//             image: "gcr.io/balmy-limiter-431522-u3/my-image:latest",
-//           },
-//         ],
-//       },
-//     },
-//   });
-// })();
